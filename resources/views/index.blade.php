eba917103612dbac685dfd396bf017168031e48a
xJustinJPD <justinpd97@gmail.com>
2023-04-09 02:04:15 +0100

    added code for card printing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,6 +16,25 @@
 
                     {{ __('You are logged in!') }}
                 </div>
+
+                @forelse ($cars as $car)
+            <div class="">
+                <a href="{{ route('teams.show', $team->id) }}">{{ $team->name }}
+                    <img src="{{asset('storage/images/' . $team->team_image) }}" width="150" />
+                
+                
+
+                <h2 class=""> 
+                    <a href="{{ route('user.cars.show', $car->id) }}">{{ $team->name }}</a>
+                </h2>
+                </a>
+            </div>
+
+            {{-- empty function incase of user having no notes --}}
+            @empty
+            <p class="font-bold text-5xl">No cars to display.</p> 
+        @endforelse
+
             </div>
         </div>
     </div>
